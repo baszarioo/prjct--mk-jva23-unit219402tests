d2bea3bc296a639440a60c36c56ad42c263f8dc8
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-06-16 12:26:04 +0200

    Update RatingRepositoryTests.java
    FindById, UpdateRating, and Delete one.

--- src/test/java/com/tattoostudio/api/repository/RatingRepositoryTests.java
+++ src/test/java/com/tattoostudio/api/repository/RatingRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -34,5 +35,31 @@ public class RatingRepositoryTests {
         List<Rating> ratingList=ratingRepository.findAll();
         Assertions.assertThat(ratingList).isNotNull();
         Assertions.assertThat(ratingList.size()).isEqualTo(2);
+    }
+    @Test
+    public void RatingRepository_FindById_ReturnsSavedRating() {
+        Rating rating = Rating.builder().title("title").content("content").stars(5).build();
+        ratingRepository.save(rating);
+        Rating ratingReturn = ratingRepository.findById(rating.getId()).get();
+        Assertions.assertThat(ratingReturn).isNotNull();
+    }
+    @Test
+    public void RatingRepository_UpdateRating_ReturnRating() {
+        Rating rating = Rating.builder().title("title").content("content").stars(5).build();
+        ratingRepository.save(rating);
+        Rating ratingSave=ratingRepository.findById(rating.getId()).get();
+        ratingSave.setTitle("title");
+        ratingSave.setContent("content");
+        Rating updatedStudio = ratingRepository.save(ratingSave);
+        Assertions.assertThat(updatedStudio.getTitle()).isNotNull();
+        Assertions.assertThat(updatedStudio.getContent()).isNotNull();
+    }
+    @Test
+    public void RatingRepository_RatingDelete_RetrunRatingIsEmpty() {
+        Rating rating = Rating.builder().title("title").content("content").stars(3).build();
+        ratingRepository.save(rating);
+        ratingRepository.deleteById(rating.getId());
+        Optional<Rating> ratingReturn = ratingRepository.findById(rating.getId());
+        Assertions.assertThat(ratingReturn).isEmpty();
     }
 }
