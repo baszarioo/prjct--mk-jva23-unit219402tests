92e1108011dc4e4028b2c2f9f8afc34d1f34fe12
baszarioo <113721856+baszarioo@users.noreply.github.com>
2023-06-16 17:29:00 +0200

    MOCKITO TESTING STUDIO SERVICE #1

--- src/test/java/com/tattoostudio/api/service/StudioServiceTests.java
+++ src/test/java/com/tattoostudio/api/service/StudioServiceTests.java
@@ -5,11 +5,19 @@ import com.tattoostudio.api.dto.StudioResponse;
 import com.tattoostudio.api.models.Studio;
 import com.tattoostudio.api.repository.StudioRepository;
 import com.tattoostudio.api.service.impl.StudioServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class StudioServiceTests {
@@ -23,6 +31,38 @@ public class StudioServiceTests {
                 .name("tattoo_bapka")
                 .city("manchester").build();
         StudioDto studioDto = StudioDto.builder().name("tatto-bapka").city("manchester").build();
-        ///to finish...
+
+    when(studioRepository.save(Mockito.any(Studio.class))).thenReturn(studio);
+            StudioDto savedStudio = studioService.createStudio(studioDto);
+            Assertions.assertThat(savedStudio).isNotNull();
     }
+    @Test
+    public void StudioService_GetAllStudios_ReturnsResponseDto() {
+        Page<Studio> studios = Mockito.mock(Page.class);
+when(studioRepository.findAll(Mockito.any(Pageable.class))).thenReturn(studios);
+        StudioResponse saveStudio = studioService.getAllStudio(1,10);
+        Assertions.assertThat(saveStudio).isNotNull();
+    }
+    ////////////////////////////////////////////////////////////////////////
+    @Test
+    public void StudioService_FindById_ReturnStudioDto() {
+        int studioId=1;
+        Studio studio = Studio.builder().id(1).name("tattoo-bapka").city("manchester").city("this is a city").build();
+        when(studioRepository.findById(studioId)).thenReturn(Optional.ofNullable(studio));
+        StudioDto studioReturn = studioService.getStudioById(studioId);
+        Assertions.assertThat(studioReturn).isNotNull();
+    }
+    @Test
+    public void StudioService_UpdateStudio_ReturnStudioDto() {
+        int studioId=1;
+        Studio studio = Studio.builder().id(1).name("tattoo-dziadu").city("londyn")
+                .city("this is a city").build();
+        StudioDto studioDto=StudioDto.builder().id(1).name("tattoo").city("londyn")
+                .city("this is a city").build();
+    when(studioRepository.findById(studioId)).thenReturn(Optional.ofNullable(studio));
+    when(studioRepository.save(studio)).thenReturn(studio);
+        StudioDto updateReturn= studioService.updateStudio(studioDto, studioId);
+        Assertions.assertThat(updateReturn).isNotNull();
+    }
+
 }
